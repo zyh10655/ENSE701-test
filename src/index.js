729af2e9919f15d691920402b09e7f12d2338802
zyh10655 <49866374+zyh10655@users.noreply.github.com>
2020-08-07 10:54:25 +1200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function addNum(a,b){
+    return a+b - 1;
+}
+module.exports=addNum;
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
