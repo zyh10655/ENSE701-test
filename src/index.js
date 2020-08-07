cc5f79861a248e46482e4a4b4a0d5fba625bce0b
NokiaZ <49866374+zyh10655@users.noreply.github.com>
2020-08-08 11:02:46 +1200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 function addNum(a,b){
     return a+b - 1;
 }
 module.exports=addNum;
 
+const Hello = (props) => {
+    return (
+      <div>
+        <p>
+          Hello {props.name}, you are {props.age} years old
+        </p>
+      </div>
+    )
+  }
+  
+  const App = () => {
+    const name = 'Peter'
+    const age = 10
+  
+    return (
+      <div>
+        <h1>Greetings</h1>
+        <Hello name="Maya" age={26 + 10} />
+        <Hello name={name} age={age} />
+      </div>
+    )
+  }
+ReactDOM.render(<App />, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
